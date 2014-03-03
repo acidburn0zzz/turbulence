69ddb62ccc35473cccfd8e3b3b7389b5def967ce
Joshua <sesshomaru98@hushmail.com>
2014-03-03 21:03:24 +0000

    Removed debugging flags

--- GUI_/MainGui_manjaroGrey.py
+++ GUI_/MainGui_manjaroGrey.py
@@ -14,9 +14,6 @@ plasmaStatus = utils_detector.detectPlasma() #Plasma
 nitrogenStatus = utils_detector.detectNitrogen() #Nitrogen
 openboxStatus = utils_detector.detectOpenBox() #Openbox
 kdeStatus = utils_detector.detectKde() #Kde
-nitrogenStatus = True
-tintStatus = True
-openboxStatus = True
 
 #Configure normal widgets
 def widgetConfigurer(widgetType, xPos, yPos, xSize, ySize, name, image=None, styleSheet=None):
